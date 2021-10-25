06b6724bb5713a7935ceb92700b61f54a73ee679
Dennis <dennispaixaoads@gmail.com>
2021-10-25 13:38:32 -0300

    menu

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 
+const RangeTamMin = document.querySelector("#range-tamanho-bolinha-min");
+const RangeTamMax = document.querySelector("#range-tamanho-bolinha-max");
+const VelocidadeQueda = document.querySelector("#range-velocidade-queda");
+const VelocidadeCriacao = document.querySelector("#range-velocidade-criacao");
+const min = parseInt(localStorage.getItem("minBolSize"));
+const max = parseInt(localStorage.getItem("maxBolSize"));
+const spCreation = localStorage.getItem("speedBolCreation");
+const spFalling = localStorage.getItem("speedFalling");
+RangeTamMin.value = min;
+RangeTamMax.value = max;
+VelocidadeCriacao.value =  parseInt(VelocidadeCriacao.getAttribute('max'))-spCreation ;
+VelocidadeQueda.value =  parseInt(VelocidadeQueda.getAttribute('max'))-spFalling ;
 const configuracao = {
-    minBolSize: 3,//tamanho minimo das bolas
-    maxBolSize: 30,//tamanho máximo das bolas em px
+    minBolSize: min?min:10,//tamanho minimo das bolas
+    maxBolSize: max?max:10,//tamanho máximo das bolas em px
     turnos: 0, // quantidade de turnos 0 para infinito
     qtBolPerTime: 1, //quantidade de bolas criadas ao mesmo tempo
-    speedBolCreation: 100,//velocidade de criaçao das bolas em ms
-    speedFalling:100,//velocidade da queda em ms
+    speedBolCreation: spCreation?spCreation:100,//,//velocidade de criaçao das bolas em ms
+    speedFalling:spFalling?spFalling:10,//velocidade da queda em ms
     speedWind:1,//velocidade do vento 
     windChance:10, //quanto maior o numero menor a chance de ventar
     speedBolDissapear:1000, //tempo até desaparecer depois que chega no chão
@@ -29,7 +41,7 @@ const configuracao = {
 
 const canvas = document.querySelector('.Canvas');
 
-function pingoDeChuva(minBolSize, maxBolSize) {
+function pingoDeChuva() {
    
     var windowWidth = canvas.clientWidth;
     var windowHeight = canvas.clientHeight;
@@ -37,7 +49,7 @@ function pingoDeChuva(minBolSize, maxBolSize) {
 
     const gota = document.createElement('div');
     gota.setAttribute('class', 'gota');
-    var size = Math.floor(Math.random() * (maxBolSize - minBolSize) + minBolSize);
+    var size = Math.floor(Math.random() * (configuracao.maxBolSize - configuracao.minBolSize) + configuracao.minBolSize);
     var maxWidth = windowWidth - size;
 
     var r = Math.floor(Math.random() * (configuracao.colors.r.max - configuracao.colors.r.min) + configuracao.colors.r.min);
@@ -58,7 +70,7 @@ function pingoDeChuva(minBolSize, maxBolSize) {
 
         let marginTopGota = 0;
         let marginLeftGota = 0;
-        let incMarginTop = 20;
+        let incMarginTop = 10;
         while (await new Promise(resolve => setTimeout(() => resolve(true), configuracao.speedFalling))
             + true) {
 
@@ -77,7 +89,7 @@ function pingoDeChuva(minBolSize, maxBolSize) {
             }
            
             //para o efeito de vento
-            let v = Math.round(Math.random() * (configuracao.windChance - 0) + 0) >= 1 ? 0 : configuracao.speedWind;//random de 0 e 1
+            let v = Math.round(Math.random() * (configuracao.windChance - 0) + 0) >= 1 ? 0 : configuracao.speedWind;
             marginLeftGota += v;
             let marginLeftRight = (parseInt(gota.style['marginLeft'].split('p')[0]));
             gota.style['marginLeft'] = (marginLeftRight + marginLeftGota) + 'px';
@@ -96,14 +108,17 @@ function pingoDeChuva(minBolSize, maxBolSize) {
 
 function criaGotas(){
     for (let i = 0; i < configuracao.qtBolPerTime; i++) {
-        pingoDeChuva(configuracao.minBolSize, configuracao.maxBolSize);
+        pingoDeChuva();
     }
 }
 
-(async () => {
+async function chover() {
     if (configuracao.turnos == 0) {
-        setInterval(function(){criaGotas()}, configuracao.speedBolCreation);
-  
+        setInterval(function(){
+            
+            criaGotas()
+        }, configuracao.speedBolCreation);
+        
     } else {
         let i = 0;
         while (await new Promise(resolve => setTimeout(() => resolve(i++), configuracao.speedBolCreation)) < configuracao.turnos) {
@@ -111,7 +126,41 @@ function criaGotas(){
         }
 
     }
-})();
+};
 
-s
+chover();
+RangeTamMin.addEventListener("click",()=>{
+ configuracao.minBolSize = parseInt(RangeTamMin.value);
+
+ localStorage.setItem("minBolSize", RangeTamMin.value);
+
+})
+
+RangeTamMax.addEventListener("click",()=>{
+    configuracao.maxBolSize = parseInt(RangeTamMax.value);
+    localStorage.setItem("maxBolSize", RangeTamMax.value);
+
+   })
+VelocidadeQueda.addEventListener("click",()=>{
+    let velocidade =parseInt(VelocidadeQueda.value);
+    let max = parseInt(VelocidadeQueda.getAttribute('max'))
+    configuracao.speedFalling =max-velocidade;
+    localStorage.setItem("speedFalling", max-velocidade);
+
+
+   
+   })
+VelocidadeCriacao.addEventListener("click",()=>{
+    let velocidade =parseInt(VelocidadeCriacao.value);
+    let max = parseInt(VelocidadeCriacao.getAttribute('max'))
+    configuracao.speedBolCreation =max-velocidade;
+    localStorage.setItem("speedBolCreation", max-velocidade);
+
+    window.location.href = '';
+    
+   
+   })   
+
+
+   
 
